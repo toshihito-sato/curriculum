c40d1cfbed4132ce623617b05336733860b5e78e
toshihito-sato <sato.sugar17@gmail.com>
2021-06-25 19:49:55 +0900

    課題提出
    2－１

--- javastudy/2-1/src/study/Main.java
+++ javastudy/2-1/src/study/Main.java
@@ -42,7 +42,7 @@ public class Main {
    //   引数には①で定義したfirstNameとlastNameを引数で受け取り、
    //  last_name と first_name を連結した値を返り値とする変数を定義しなさい
   public static String getName(String firstName,String lastName) {
-	   return "first_name" + "last_name ";
+	   return "first_name"+" "+"last_name ";
    }
 
 
